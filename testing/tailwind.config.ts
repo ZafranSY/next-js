18e4a094f1d2dbfea4ae5146aa4bbe0c5af61191
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-02-15 18:03:41 +0800

    Update tailwind.config.ts

--- testing/tailwind.config.ts
+++ testing/tailwind.config.ts
@@ -8,9 +8,26 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "15px",
+      },
       colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray: {
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          400: "#d4d7dc",
+          300: "#e3e5e8",
+          200: "#ebedef",
+          100: "#f2f3f5",
+        },
       },
     },
   },
